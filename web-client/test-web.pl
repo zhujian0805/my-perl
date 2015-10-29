a22514e141220136ab53cb5b64734a1db0ca4065
James Zhu <zhujian0805@gmail.com>
2015-10-29 13:51:56 +0800

    M web-client/test-web.pl

--- web-client/test-web.pl
+++ web-client/test-web.pl
@@ -3,9 +3,9 @@ use strict;
 use LWP 5.64;
 
 my $browser = LWP::UserAgent->new;
-my $url = 'http://www.baidu.com';
+my $url = 'http://10.24.0.242:8085';
 my $response = $browser->get($url);
 
-die "Error: ", $response->header('WWW-Authenticate') || 
-    'Error accessing', "\n ", $response->status_line,
-    "\n at $url\n Aborting" unless $response->is_success;
+die "Error:\n ", $response->header('WWW-Authenticate') || 
+    "\nError accessing: \n", $response->status_line,
+    "\n at $url\n Aborting\n" unless $response->is_success;
