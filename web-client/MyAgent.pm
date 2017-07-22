3573e447de240c7374079dca4dc8170ce4118bcd
James Zhu <zhujian0805@gmail.com>
2017-07-22 13:19:05 +0800

    This commit has the following change(s): ------------------------------------------------  M MyAgent.pm ------------------------------------------------

--- web-client/MyAgent.pm
+++ web-client/MyAgent.pm
@@ -1,25 +1,25 @@
 #Overriding LWP::UserAgent
 use LWP::UserAgent;
+
 package MyAgent;
- 
+
 use vars qw(@ISA);
 @ISA = qw(LWP::UserAgent);
- 
+
 my ( $username, $password ) = ( undef, undef );
- 
+
 # method to set username and password for authentication
 sub set_credentials {
-  shift;
-  ( $username, $password ) = @_;
+    shift;
+    ( $username, $password ) = @_;
 }
- 
+
 # this routine gets called when your browser
 #   would otherwise be asked to provide a
 #   username and password
 sub get_basic_credentials {
     shift;
-    return( $username, $password );
+    return ( $username, $password );
 }
-
 
 1;
