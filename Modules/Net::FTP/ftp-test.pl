25a704d2eec9491b1aa449c0774391c2fd120bf6
James Zhu <zhujian0805@gmail.com>
2017-07-30 20:20:01 +0800

    This commit has the following change(s): ------------------------------------------------ M  ftp-test.pl ------------------------------------------------

--- Modules/Net::FTP/ftp-test.pl
+++ Modules/Net::FTP/ftp-test.pl
@@ -22,13 +22,22 @@ use strict;
 use warnings;
 use utf8;
 use Net::FTP;
+use Try::Tiny qw(try catch);
 
 my $ftp = Net::FTP->new("localhost") or die "Cannot connect to some.host.name: $@";
 
 $ftp->login("jzhu", "123456");
 
+print "Those files are in /tmp\n";
+print "----------------------------\n";
 foreach my $dir ( $ftp->ls("/tmp") ) {
     print $dir, "\n";
 }
 
-$ftp->get("/tmp/testing-ftp", "/tmp/testing-ftp-got");
+my $ret = try {
+    $ftp->get("/tmp/testing-ftp", "/tmp/testing-ftp-got") or die "$@";
+} catch {
+    "Failed to get file $@\n";
+};
+
+print "Catched: ", $ret, "\n";
